27a698f11ec60ef4a2efd287dedc2b17952787ad
plejus <artur.rychcik@gmail.com>
2019-08-07 20:52:37 +0200

    Initial commit

--- src/Inflector.php
+++ src/Inflector.php
@@ -7,8 +7,39 @@
 
 namespace plejus\PhpPluralize;
 
+use plejus\PhpPluralize\Rules\IrregularPlurals;
+use plejus\PhpPluralize\Rules\PluralizationRule;
+use plejus\PhpPluralize\Rules\SingularizationRule;
+use plejus\PhpPluralize\Rules\UncountableRule;
+
 class Inflector
 {
+    private $irregularSingles = [];
+    private $irregularPlurals = [];
+    private $pluralRules      = [];
+    private $singularRules    = [];
+    private $uncountables     = [];
+
+    public function __construct()
+    {
+        $this->pluralRules[]   = PluralizationRule::getAll();
+        $this->singularRules[] = SingularizationRule::getAll();
+
+        foreach (IrregularPlurals::getAll() as $rule) {
+            $this->irregularSingles[$rule[0]] = $rule[1];
+            $this->irregularPlurals[$rule[1]] = $rule[0];
+        }
+
+        foreach (UncountableRule::getAll() as $rule) {
+            if (substr($rule, 0, 1) === '/') {
+                $this->pluralRules[]   = [$rule, '$0'];
+                $this->singularRules[] = [$rule, '$0'];
+            } else {
+                $this->uncountables[] = $rule;
+            }
+        }
+    }
+
     public function pluralize($text, $occurences = 2)
     {
         return $text;
@@ -27,5 +58,42 @@ class Inflector
     public function isPlural($text)
     {
         return false;
+    }
+
+    /**
+     * @param $word
+     * @param $token
+     *
+     * @return string
+     */
+    private function restoreCase($word, $token)
+    {
+        // Tokens are an exact match.
+        if ($word === $token) {
+            return $token;
+        }
+
+        // Lower cased words. E.g. "hello".
+        if ($word === strtolower($word)) {
+            return strtolower($token);
+        }
+
+        // Upper cased words. E.g. "WHISKY".
+        if ($word === strtoupper($word)) {
+            return strtoupper($token);
+        }
+
+        // Title cased words. E.g. "Title".
+        if ($word === ucfirst($word)) {
+            return ucfirst($token);
+        }
+
+        // Lower cased words. E.g. "test".
+        return strtolower($token);
+    }
+    
+    private function interpolate($str, $args)
+    {
+        return preg_replace('/\$(\d{1,2})/g', $args, $str);
     }
 }
---
+++ src/Rules/IrregularPlurals.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * pluralize.
+ * Author: Artur Rychcik (artur.rychcik@gmail.com)
+ * 07.08.2019, 17:56
+ */
+
+namespace plejus\PhpPluralize\Rules;
+
+/**
+ * Class IrregularPlurals
+ *
+ * @package plejus\PhpPluralize\Rules
+ */
+class IrregularPlurals
+{
+    /**
+     * @return array
+     */
+    public static function getAll()
+    {
+        return [
+            // Pronouns.
+            ['I', 'we'],
+            ['me', 'us'],
+            ['he', 'they'],
+            ['she', 'they'],
+            ['them', 'them'],
+            ['myself', 'ourselves'],
+            ['yourself', 'yourselves'],
+            ['itself', 'themselves'],
+            ['herself', 'themselves'],
+            ['himself', 'themselves'],
+            ['themself', 'themselves'],
+            ['is', 'are'],
+            ['was', 'were'],
+            ['has', 'have'],
+            ['this', 'these'],
+            ['that', 'those'],
+            // Words ending in with a consonant and `o`.
+            ['echo', 'echoes'],
+            ['dingo', 'dingoes'],
+            ['volcano', 'volcanoes'],
+            ['tornado', 'tornadoes'],
+            ['torpedo', 'torpedoes'],
+            // Ends with `us`.
+            ['genus', 'genera'],
+            ['viscus', 'viscera'],
+            // Ends with `ma`.
+            ['stigma', 'stigmata'],
+            ['stoma', 'stomata'],
+            ['dogma', 'dogmata'],
+            ['lemma', 'lemmata'],
+            ['schema', 'schemata'],
+            ['anathema', 'anathemata'],
+            // Other irregular rules.
+            ['ox', 'oxen'],
+            ['axe', 'axes'],
+            ['die', 'dice'],
+            ['yes', 'yeses'],
+            ['foot', 'feet'],
+            ['eave', 'eaves'],
+            ['goose', 'geese'],
+            ['tooth', 'teeth'],
+            ['quiz', 'quizzes'],
+            ['human', 'humans'],
+            ['proof', 'proofs'],
+            ['carve', 'carves'],
+            ['valve', 'valves'],
+            ['looey', 'looies'],
+            ['thief', 'thieves'],
+            ['groove', 'grooves'],
+            ['pickaxe', 'pickaxes'],
+            ['passerby', 'passersby']
+        ];
+    }
+}
